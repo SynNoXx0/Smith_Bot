2fb388bc9922e837ce45abc9bcec50aff2ba178f
SynNoXx0 <synnoxx0@gmail.com>
2025-05-12 23:49:41 +0200

    Add files via upload

--- utils/decorators.py
+++ utils/decorators.py
@@ -21,21 +21,15 @@ def check_permissions(command_name: str):
 
 def check_public_permissions(command_name: str):
     async def predicate(interaction: Interaction):
-        # Vérifier si des permissions sont définies pour cette commande
+        # Récupérer les rôles de l'utilisateur
         user_role_ids = [role.id for role in interaction.user.roles]
-        has_permissions = is_role_allowed_for_command(str(interaction.guild.id), command_name)
         
-        # Si aucune permission n'est définie (has_permissions est False ou None), autoriser l'utilisation
-        if has_permissions is False or has_permissions is None:
+        # Vérifier si l'utilisateur a un rôle autorisé pour la commande
+        if is_role_allowed_for_command(str(interaction.guild.id), command_name, user_role_ids):
             return True
-        
-        # Si has_permissions est True (permissions définies), vérifier si l'utilisateur a le rôle requis
-        if isinstance(has_permissions, list):  # Vérifie si has_permissions est une liste de rôles autorisés
-            if not any(str(role_id) in has_permissions for role_id in user_role_ids):
-                raise app_commands.CheckFailure("Tu n'as pas le rôle requis pour utiliser cette commande.")
-        
-        # Si des permissions sont définies et l'utilisateur a le rôle requis, autoriser
-        return True
+
+        # Sinon, on refuse
+        raise app_commands.CheckFailure("Tu n'as pas la permission d'utiliser cette commande.")
 
     return app_commands.check(predicate)
 
